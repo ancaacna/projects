89d121fbc747b7b4efb4a6e57fd65cc723eca95b
Desmiansah Anca <desmiansah.anca@gmail.com>
2022-03-20 22:19:15 +0700

    push

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+dibuat menggunakan Framework Codeigniter (CI)
+
+user admin :
+u: admin
+p: admin
+
+database :
+buat database dengan nama jobs dan silahkan import(dump sql) kedalama database jobs
---
+++ jobs_recruitment/application/views/page/partner.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/jam.css">
+<div class="row mt-4">
+	<div class="col-md-9">
+		<div class="card">
+			<div class="card-body">
+			   <img src="<?= base_url() ?>/assets/img/img-index.png" width="100%">
+				<h4>Partner Bisnis</h4>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,lpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card">
+			<div class="card-header">Menu Navigasi</div>
+
+			<ul class="list-group list-group-flush">
+			    <li class="list-group-item"><a href="<?= base_url('page') ?>">Sejarah Perusahaan</a></li>
+			    <li class="list-group-item"><a href="<?= base_url('page/visi_misi') ?>">Visi & Misi </a></li>
+			    <li class="list-group-item"><a href="<?= base_url('page/partner') ?>">Partner Bisnis</a></li>
+			</ul>
+		</div>	
+	</div>
+</div>
